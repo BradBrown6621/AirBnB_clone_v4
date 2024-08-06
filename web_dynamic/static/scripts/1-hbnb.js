8cc8a63fac6853abfcce5386d9bc83ea7f22c078
BradBrown6621 <6621@holbertonstudents.com>
2024-08-06 18:12:14 -0500

    web_dynamic/static/scripts/1-hbnb.js: Implemented dynamic display of selected amenities web_dynamic/templates/1-hbnb.html: See above

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/node
 
 $(function () {
@@ -11,30 +10,3 @@ $(function () {
     $('.amenities h4').text(Object.values(checkedAmenities).join(', '));
   });
 });
-=======
-$(document).ready(function() {
-    let checkedAmenities = [];
-
-    $('input[type="checkbox"]').on('change', function() {
-        let amenityId = $(this).data('id');
-        
-        if ($(this).is(':checked')) {
-            if (!checkedAmenities.includes(amenityId)) {
-                checkedAmenities.push(amenityId);
-            }
-        } else {
-            checkedAmenities = checkedAmenities.filter(id => id !== amenityId);
-        }
-
-        updateAmenitiesHeader();
-    });
-
-    function updateAmenitiesHeader() {
-        let checkedAmenitiesText = checkedAmenities.map(id => {
-            return $(`input[data-id="${id}"]`).next('label').text();
-        }).join(', ');
-
-        $('#amenities-header').text(`Amenities: ${checkedAmenitiesText}`);
-    }
-});
->>>>>>> 6bd87d47b8d0bb6fe2e894fd61f615a8480a4546
